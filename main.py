5262ccaadfe9e9b04f59624a71ecd5630cdbdcc3
Nouman Mahmood <46532439+noumanmhd@users.noreply.github.com>
2020-09-07 23:52:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -69,40 +69,8 @@ class CaptureFrame(QtCore.QObject):
             data = cv2.imread(self.image_path)
             read = True
         if read:
-            data = resize(data, height=480)
-            data = resize(data, width=540)
+            data = cv2.resize(data, (540, 340), interpolation=cv2.INTER_AREA)
             self.image_data.emit(data)
-
-def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
-
-    # return the resized image
-    return resized
 
 class VideoStream(QtWidgets.QWidget):
     def __init__(self, parent=None):
